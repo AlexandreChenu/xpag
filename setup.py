3fe33d8289a063b70441af8c5abeb24ab58b394f
Alexandre Chenu <alexandre.chenu@ensta-bretagne.org>
2024-11-10 19:56:50 +0100

    updated version 2024

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import setup, find_packages
 
 # Install with 'pip install -e .'
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="xpag",
-    version="0.1.0",
+    version="0.2.9",  # -version-
     author="Nicolas Perrin-Gilbert",
     description="xpag: Exploring Agents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/perrin-isir/xpag",
     packages=find_packages(),
     install_requires=[
@@ -14,10 +21,16 @@ setup(
         "numpy>=1.21.5",
         "matplotlib>=3.1.3",
         "joblib>=1.1.0",
-        "gym>=0.22.0",
-        "torch>=1.10.0",
+        "gymnasium>=0.28.1",
         "Pillow>=9.0.1",
         "ipywidgets>=7.6.5",
+        "jax>=0.4.8",
+        "optax>=0.1.2",
+        "flax>=0.7.2",
+        "dm-haiku>=0.0.10",
+        "brax>=0.9.1",
+        "tensorflow-probability>=0.15.0",
+        "mediapy>=1.1.4",
     ],
     license="LICENSE",
 )
